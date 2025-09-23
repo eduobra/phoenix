6bc64427b076f90f540f832add2331cc3d05881f
Eugine Rosillon <bugineeugine06@gmail.com>
2025-09-23 15:03:51 +0800

    new branch

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -129,10 +129,10 @@ export default function LoginPage() {
 
   if (loading || redirecting) {
     return (
-       <div className="flex h-screen w-screen items-center justify-center bg-white">
+      <div className="flex items-center justify-center w-screen h-screen bg-white">
         <div className="flex space-x-2">
           <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce delay-0"></span>
-          <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce delay-200"></span>
+          <span className="w-3 h-3 delay-200 bg-gray-500 rounded-full animate-bounce"></span>
           <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce delay-400"></span>
           <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce delay-600"></span>
         </div>
